a5fbb42275af9022073b4ba13fb5f7d957e4841e
Cesar Toscano <ctoscano@bab.as>
2011-01-05 23:46:25 -0500

    updated scrips; mongod self repairs

--- scripts/mongod.sh
+++ scripts/mongod.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
+
+# First, kill off any potentially
+# running daemons by sending them all a SIGINT...
+killall -2 mongod >/dev/null 2>&1
+sleep 2
+
+# Now, if there's a lock file still present then mongod wasn't running
+# and we need to clean things up.
+if [ -f /data/db/mongod.lock ]; then
+   rm -f /data/db/mongod.lock
+   mongod --repair >/tmp/mongod.repair.log 2>&1
+fi
+
+# Now, run mongod and have it fork off to become a daemon.  
+# Logs will be discarded
 mongod --fork --logpath /dev/null --logappend
+
--- scripts/start.prefork.sh
+++ scripts/start.prefork.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-kill `cat /var/tmp/pluma.pid`
-rm /var/tmp/pluma.pid 
+if [ -f /var/tmp/pluma.pid ]; then
+    kill `cat /var/tmp/pluma.pid`
+    rm /var/tmp/pluma.pid 
+fi
+
 su www-data -c "./example/manage.py runfcgi method=prefork host=127.0.0.1 port=8000 pidfile=/var/tmp/pluma.pid"
+
